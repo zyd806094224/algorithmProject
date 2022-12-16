dbd41e2eba401bf335d6b3a16e32b528842d9553
zhaoyudong <zhaoyudong@hualala.com>
2022-12-16 18:02:12 +0800

    接雨水问题 单调栈

--- src/com/algorithm/RainwaterConnectionProblem.java
+++ src/com/algorithm/RainwaterConnectionProblem.java
@@ -1,14 +1,22 @@
 package com.algorithm;
 
+import java.util.Stack;
+
 /**
- * 接雨水问题 双指针解法
+ * 接雨水问题 双指针&单调栈解法
  */
 public class RainwaterConnectionProblem {
     public static void main(String[] args) {
         int[] height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trap(height));
+        System.out.println(trap2(height));
     }
 
+    /**
+     * 双指针
+     * @param height
+     * @return
+     */
     public static int trap(int[] height) {
         int ans = 0;
         int leftMax = 0;
@@ -35,5 +43,36 @@ public class RainwaterConnectionProblem {
             }
         }
         return ans;
+    }
+
+    /**
+     * 单调栈
+     * @param height
+     * @return
+     */
+    public static int trap2(int[] height) {
+        int len = height.length;
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        int ans = 0;
+        while(index < len){
+            while(index < len && (stack.isEmpty() || height[stack.peek()] >= height[index])){
+                stack.push(index++);
+            }
+            if(index == len){
+                break;
+            }
+            while(!stack.isEmpty() && height[stack.peek()] < height[index]){
+                int currIndex = stack.pop();
+                if(stack.isEmpty()){
+                    break;
+                }
+                int w = index - stack.peek() - 1;
+                int h = Math.min(height[index],height[stack.peek()]) - height[currIndex];
+                ans = ans + w * h;
+            }
+        }
+        return ans;
+
     }
 }
